fb3717cf91238db7e41736ea106209a8802d97d7
esionascimento <esio.392@protonmail.com>
2021-12-16 18:06:46 -0400

    inserido import do register

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { Dashboard } from './pages/dashboard/Dashboard';
+import { Register } from './components/register';
 
 import './App.css';
 
@@ -9,7 +10,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Dashboard />} />
+        <Route exact path="/" element={<Register />} />
+        <Route exact path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
