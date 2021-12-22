d906afedfec3265500c80d89bfedc8460d354359
esionascimento <esio.392@protonmail.com>
2021-12-22 17:30:20 -0400

    criado redux para login e logout

---
+++ src/store/auth/authReducer.js
@@ -0,0 +1,19 @@
+const INITIAL_STATE = {
+  isAuthenticated: false
+};
+
+
+export default function AuthReducer (state = INITIAL_STATE, action) {
+  switch(action.type) {
+    case 'LOGIN':
+      return {
+        ...state, isAuthenticated: true
+      }
+    case 'LOGOUT':
+      return {
+        ...state, isAuthenticated: false
+      }
+    default:
+      return state;
+  }
+};
