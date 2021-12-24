ba4ed94686df4f3b4d9445e6b822e158fa978195
esionascimento <esio.392@protonmail.com>
2021-12-24 19:05:09 -0400

    refatora endpoint list_products e criacao de headers

--- src/services/fetchActionIfood.js
+++ src/services/fetchActionIfood.js
@@ -5,11 +5,15 @@ const BASE_URL = process.env.REACT_APP_BASE_URL;
 const tokenIfood = localStorage.getItem('tokenIfood');
 
 const APIPOST = axios.create({
-  baseURL: BASE_URL
+  baseURL: BASE_URL,
+  headers: {
+    'Host': 'merchant-api.ifood.com.br',
+    'Authorization': `Bearer ${tokenIfood}`
+  }
 });
 
 export const fechtAuthenticationCode = (user) => APIPOST.post('/authentication/usercode', user);
 
 export const fechtAuthenticationTokenCentralized = () => APIPOST.get('/authentication/token/centralized');
 
-export const fechtMerchantCatalogProductList = () => APIPOST.post('/merchant/list_products');
+export const fechtMerchantCatalogProductList = () => APIPOST.get('/merchant/catalog/list_products');
