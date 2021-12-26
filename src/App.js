8d65037136bc536b4ea1d700f6c323ea5acb746b
esionascimento <esio.392@protonmail.com>
2021-12-25 21:22:09 -0400

    adicionado dois imports

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Dashboard } from './pages/dashboard/Dashboard';
 import { Register } from './components/register';
 import { Home } from './pages/home';
+import { NotFound } from './pages/notFound/notFound'
+import { RoutePrivate } from './services/privateLogin'
 
 import './App.css';
 
@@ -11,9 +13,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route exact path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route exact path="/register" element={<Register />} />
-        <Route exact path="/dashboard" element={<Dashboard />} />
+        <Route exact path="/dashboard" element={<RoutePrivate />} >
+          <Route path="" element={<Dashboard />} />
+        </Route>
+        <Route exact path="/not-found" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
