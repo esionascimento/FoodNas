4cf9ecc44d1041e8bf77f150880fdc643c731a62
esionascimento <esio.392@protonmail.com>
2021-12-31 21:08:05 -0400

    refatora authentication

--- src/utils/withAuth.tsx
+++ src/utils/withAuth.tsx
@@ -23,9 +23,30 @@ export default function withAuth(WrappedComponent: ElementType, props) {
   return Wrapper;
 }
 
-export async function getServerSideProps(context) {
+/* export async function getServerSideProps(context) {
   const cookies = parseCookies(context);
   console.log('cookies :', cookies);
+  return {
+    props: {
+      IFOOD_TOKEN: cookies['ifood.token'] ? cookies['ifood.token'] : '',
+      ATLAS_TOKEN: cookies['atlas.token'] ? cookies['atlas.token'] : ''
+    }
+  }
+} */
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const cookies = context;
+  console.log('cookies :', cookies);
+
+  if (!cookies) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+
   return {
     props: {
       IFOOD_TOKEN: cookies['ifood.token'] ? cookies['ifood.token'] : '',
