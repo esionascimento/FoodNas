72e500cdbbb96bda8c5501191220b77d8db009f3
esionascimento <esio.392@protonmail.com>
2022-01-07 17:15:19 -0400

    refatora criado novas rotas | criado propriedade icon + imports de icons

--- src/components/left-menu/options.js
+++ src/components/left-menu/options.js
@@ -1,25 +1,50 @@
+import {
+  HomeOutlined,
+  SettingFilled,
+  LogoutOutlined,
+  BugOutlined,
+  ProfileOutlined
+} from '@ant-design/icons';
+
 export const iconsListAdmin = [
   {
-      name: "dashboard",
-      text: "Dashboard",
-      path: "/admin/dashboard",
-      active: true,
-      access: true,
+    name: "dashboard",
+    text: "Dashboard",
+    path: "/admin/dashboard",
+    active: true,
+    access: true,
+    icon: <HomeOutlined />
   },
   {
-      name: "documentacao",
-      text: "Login",
-      path: "https://documenter.getpostman.com/view/8141632/Tz5qZGZR#bf94bad1-6090-4ee5-9839-ef309e533d0a",
-      active: true,
-      access: true,
+    name: "profile",
+    text: "Perfil",
+    path: "https://documenter.getpostman.com/view/8141632/Tz5qZGZR#bf94bad1-6090-4ee5-9839-ef309e533d0a",
+    active: true,
+    access: true,
+    icon: <ProfileOutlined />
   },
   {
-      name: "graficos",
-      text: "Registro",
-      path: "",
-      active: false,
-      access: false,
+    name: "config",
+    text: "Configuração",
+    path: "",
+    active: false,
+    access: false,
+    icon: <SettingFilled />
   },
- 
- 
+  {
+    name: "report",
+    text: "Report",
+    path: "",
+    active: false,
+    access: false,
+    icon: <BugOutlined />
+  },
+  {
+    name: "logout",
+    text: "Sair",
+    path: "",
+    active: false,
+    access: false,
+    icon: <LogoutOutlined />
+  },
 ];
