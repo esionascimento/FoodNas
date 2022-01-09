0335a610f0e5b20f5f2afbc3f90081aade0ed6d9
esionascimento <esio.392@protonmail.com>
2022-01-08 23:25:21 -0400

    refatora adicionado sider a pagina

--- src/pages/report/index.tsx
+++ src/pages/report/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import LeftMenu from "../../components/left-menu/index";
+import {Layout} from "antd";
+const { Footer } = Layout;
+import 'antd/dist/antd.css';
 import withAuth from '../../utils/withAuth';
 
 function Report() {
 
   return (
-    <p>Report</p>
+    <Layout style={{ minHeight: '100vh' }}>
+      {typeof  window  ! ==  'undefined' ? 'null' : 
+        <LeftMenu />
+      }
+      <Layout>
+        <div>
+          Report
+        </div>
+        <Footer style={{ textAlign: 'center' }}>...</Footer>
+      </Layout>
+    </Layout>
   );
 }
 
