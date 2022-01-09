8d0db9585fc9ab682aabb7fef1cdce1be46794a6
esionascimento <esio.392@protonmail.com>
2022-01-08 23:21:07 -0400

    refatora onClick para selecionar o menuItem selecional no momento

--- src/components/left-menu/index.js
+++ src/components/left-menu/index.js
@@ -3,27 +3,41 @@ import * as S from "./styled";
 import Link from "next/link";
 import { Tooltip, Menu } from "antd";
 import { iconsListAdmin } from "./options";
+import { setCookie, parseCookies } from 'nookies';
+import cookiess from 'nookies';
 import 'antd/dist/antd.css';
 
 function LeftMenu() {
   const [collapsed, setCollapsed] = useState(true);
+  const { 'food.sider.index': foodSiderIndex } = parseCookies();
 
-  const SiderHandle = () => {
-    setCollapsed(!collapsed);
+  const SiderHandleEnter = () => {
+    setCollapsed(false);
   };
+
+  const SiderHandleLeave = () => {
+    setCollapsed(true);
+  };
+
+  function onClick(index) {
+    setCollapsed(true);
+    setCookie(null, 'food.sider.index', index.key, {maxAge: 86400 * 7, path: '/'});
+  }
 
   return (
     <>
       <S.Sider 
         collapsible
         collapsed={collapsed}
-        onMouseEnter={SiderHandle}
-        onMouseLeave={SiderHandle}
+        onMouseEnter={SiderHandleEnter}
+        onMouseLeave={SiderHandleLeave}
       >
-        <Menu style={{ backgroundColor: '#F0F2F5' }}
+        <Menu
+          onClick={onClick}
+          /* style={{ backgroundColor: '#F0F2F5' }} */
           selectable
           defaultSelectedKeys={
-            ["0"]
+            foodSiderIndex ? "0" : foodSiderIndex
           }
           mode="inline"
         >
@@ -39,11 +53,9 @@ function LeftMenu() {
                       active && access ? "" : "Em Desenvolvimento"
                     }
                     placement="right"
-                  >
-                    <span style={{ display: "none" }} />
-                    
+                  >     
                     <S.Text>{text}</S.Text>
-                  </Tooltip>  
+                  </Tooltip>
                 </Link>
               </S.MenuItem>
             )
