adc0a3aaf464fb8a6a78ef23bd91784161679b37
esionascimento <esio.392@protonmail.com>
2022-01-10 17:05:05 -0400

    refatora css border

--- styles/dashboardCss.ts
+++ styles/dashboardCss.ts
@@ -4,11 +4,11 @@ export const DivBody = styled.div`
   height: 75vh;
   overflow: auto;
   padding: 0 16px;
-  border: 5px solid rgba(140, 140, 140, 0.35);
+  border: 1px solid rgba(140, 140, 140, 0.35);
 `;
 
 export const DivFooter = styled.div`
   height: 10vh;
   overflow: auto;
-  border: 5px solid rgba(140, 140, 140, 0.35);
+  border: 1px solid rgba(140, 140, 140, 0.35);
 `;
