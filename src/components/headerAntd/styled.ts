0385f383ca2d44eca2f504d6f4f82798c0de2fa9
esionascimento <esio.392@protonmail.com>
2022-01-15 17:38:19 -0400

    refatora styled headerAntd

---
+++ src/components/headerAntd/styled.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Div = styled.div`
+  display: flex;
+  padding: 0 5px;
+`;
+
+export const DivMenu = styled.div`
+  padding: 0 5px;
+  &:hover{
+    background-color: slateblue;
+  }
+`;
