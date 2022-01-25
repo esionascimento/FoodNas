e52295acfa04c6215884df8aea8c2ed50198ef68
esionascimento <esio.392@protonmail.com>
2022-01-25 17:40:03 -0400

    import styled | refatora css

--- src/components/layoutContent/index.tsx
+++ src/components/layoutContent/index.tsx
@@ -3,6 +3,8 @@ import { useSelector } from 'react-redux';
 
 import { fechtOrderDetails } from '../../services/FetchFood/merchantOrder';
 
+import { DivContact } from './styled';
+
 export function ComponentBody() {
   interface RootState {
     storeDashboard: {
@@ -26,13 +28,17 @@ export function ComponentBody() {
   function items() {
     return dataLog.items.map((aux: any, index: any) => {
         return (
-          <div key={index}>
-            <p>{index + 1}</p>
-            <p>{`nome: ${aux.name}`}</p>
-            <p>{`quantidade: ${aux.quantity}`}</p>
-            <p>{`valor unitario: ${aux.unitPrice !== 0 ? aux.unitPrice : aux.optionsPrice}`}</p>
-            <p>{`Valor Total: ${aux.totalPrice}`}</p>
-          </div>
+          <DivContact key={index}>
+            <div>
+              <p>{index + 1}</p>
+            </div>
+            <div>
+              <p>{`nome: ${aux.name}`}</p>
+              <p>{`quantidade: ${aux.quantity}`}</p>
+              <p>{`valor unitario: ${aux.unitPrice !== 0 ? aux.unitPrice : aux.optionsPrice}`}</p>
+              <p>{`Valor Total: ${aux.totalPrice}`}</p>
+            </div>
+          </DivContact>
         )
       })
   }
@@ -66,19 +72,16 @@ export function ComponentBody() {
     } else if (selectPedido === 'pending') {
       return (
         <>
+          {}
           <h2>Pedido Pendente</h2>
           <div>
             <h3>Contato</h3>
-            {/* <p>{`Nome: ${dataLog && dataLog.customer.name}`}</p> */}
-            <p>{`Telefone: ${dataLog && dataLog.customer.phone.number}`}</p>
-            <p>{`Localizador: ${dataLog && dataLog.customer.phone.localizer}`}</p>
+            <DivContact>
+              <p>{`Telefone: ${dataLog && dataLog.customer.phone.number}`}</p>
+              <p>{`Localizador: ${dataLog && dataLog.customer.phone.localizer}`}</p>
+            </DivContact>
             <h3>Endereço</h3>
-            <p>{`rua: ${dataLog && dataLog.delivery.deliveryAddress.streetName}`}</p>
-            {/* <p>{`numero: ${dataLog && dataLog.delivery.deliveryAddress.streetNumber}`}</p> */}
-            {/* <h3>Pagamento</h3>
-            <p>{`metodo pagamento: ${dataLog && dataLog.payments.methods[0].method}`}</p>
-            <p>{`entrega: ${dataLog && dataLog.total.deliveryFee}`}</p>
-            <p>{`total: ${dataLog && dataLog.total.orderAmount}`}</p> */}
+            <p>{`Rua: ${dataLog && dataLog.delivery.deliveryAddress.streetName}`}</p>
             <h3>Pedido(s)</h3>
             {dataLog ? items() : ''}
           </div>
