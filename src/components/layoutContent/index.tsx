933ae62d64e67273e65673d799efe6c82cc83088
esionascimento <esio.392@protonmail.com>
2022-01-26 03:50:44 -0400

    refatora layoutContent | import components pending e canceled

--- src/components/layoutContent/index.tsx
+++ src/components/layoutContent/index.tsx
@@ -5,7 +5,8 @@ import { fechtOrderDetails } from '../../services/FetchFood/merchantOrder';
 
 import { ACDataOrderDetails } from '../../store/dashboard/dashboardAction';
 
-import { DivContact } from './styled';
+import { ComponentPending } from './componentPending';
+import { ComponentCanceled } from './componentCanceled';
 
 export function ComponentBody() {
   interface RootState {
@@ -29,23 +30,6 @@ export function ComponentBody() {
     detailsOrderId();
   }, [selectOrderId, dispatch])
 
-  function items() {
-    return dataLog.items.map((aux: any, index: any) => {
-        return (
-          <DivContact key={index}>
-            <div>
-              <p>{index + 1}</p>
-            </div>
-            <div>
-              <p>{`nome: ${aux.name}`}</p>
-              <p>{`quantidade: ${aux.quantity}`}</p>
-              <p>{`valor unitario: ${aux.unitPrice !== 0 ? aux.unitPrice : aux.optionsPrice}`}</p>
-              <p>{`Valor Total: ${aux.totalPrice}`}</p>
-            </div>
-          </DivContact>
-        )
-      })
-  }
   function componentBody() {
     if (selectPedido === 'null') {
       return (
@@ -56,44 +40,18 @@ export function ComponentBody() {
     } else if (selectPedido === 'canceled') {
       return (
         <>
-          <h2>Pedido Cancelado</h2>
-          <div>
-            <h3>Contato</h3>
-            <p>{`Nome: ${dataLog && dataLog.customer.name}`}</p>
-            <p>{`Telefone: ${dataLog && dataLog.customer.phone.number}`}</p>
-            <p>{`Localizador: ${dataLog && dataLog.customer.phone.localizer}`}</p>
-            <h3>Endereço</h3>
-            <p>{`rua: ${dataLog && dataLog.delivery.deliveryAddress.streetName}`}</p>
-            <p>{`numero: ${dataLog && dataLog.delivery.deliveryAddress.streetNumber}`}</p>
-            <h3>Pagamento</h3>
-            <p>{`metodo pagamento: ${dataLog && dataLog.payments.methods[0].method}`}</p>
-            <p>{`entrega: ${dataLog && dataLog.total.deliveryFee}`}</p>
-            <p>{`total: ${dataLog && dataLog.total.orderAmount}`}</p>
-            {dataLog && items()}
-          </div>
+          {ComponentCanceled(dataLog)}
         </>
       )
     } else if (selectPedido === 'pending') {
       return (
         <>
-          {}
-          <h2>Pedido Pendente</h2>
-          <div>
-            <h3>Contato</h3>
-            <DivContact>
-              <p>{`Telefone: ${dataLog && dataLog.customer.phone.number}`}</p>
-              <p>{`Localizador: ${dataLog && dataLog.customer.phone.localizer}`}</p>
-            </DivContact>
-            <h3>Endereço</h3>
-            <p>{`Rua: ${dataLog && dataLog.delivery.deliveryAddress.streetName}`}</p>
-            <h3>Pedido(s)</h3>
-            {dataLog ? items() : ''}
-          </div>
+          {ComponentPending(dataLog)}
         </>
       )
     }
   }
-
+    
   return (
     <>
       {dataLog ? componentBody() : <p>...</p>}
