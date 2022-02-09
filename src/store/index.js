8c54a728b90c26284fad747ba75b6b5c03bcbfba
esionascimento <esio.392@protonmail.com>
2022-02-09 16:52:41 -0400

    import StoreDataOrder

--- src/store/index.js
+++ src/store/index.js
@@ -6,11 +6,13 @@ import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import { MerchantOrder } from './merchantOrder/merchantOrderReducer';
 import { AuthReducer } from './auth/authReducer';
 import { StoreDashboard } from './dashboard/dashboardReducer';
+import { StoreDataOrder } from './dataOrder/dataOrderReducer';
 
 const rootReducer = combineReducers({
   merchantOrder: MerchantOrder,
   auth: AuthReducer,
-  storeDashboard: StoreDashboard
+  storeDashboard: StoreDashboard,
+  storeDataOrder: StoreDataOrder
 });
 
 const store = () => createStore(
