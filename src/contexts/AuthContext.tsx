ee2c0da220b42554a62b2fc23f4a9063d518d856
esionascimento <esio.392@protonmail.com>
2022-02-18 00:40:34 -0400

    refatora eslint

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,7 +1,8 @@
-import { createContext, /* useEffect, */ useState } from 'react'
+import React, { createContext, /* useEffect, */ useState } from 'react'
+import { HeadersDefaults } from 'axios'
 import { fetchLogin/* , fetchAuthorizationAtlas */ } from '../services/FetchAtlas'
-import { APIATLAS } from '../services/FetchAtlas/utilsPostgres';
-import { setCookie/* , parseCookies */ } from 'nookies';
+import { APIATLAS } from '../services/FetchAtlas/utilsPostgres'
+import { setCookie/* , parseCookies */ } from 'nookies'
 
 type User = {
   name: string;
@@ -21,11 +22,15 @@ type AuthContextType = {
   signIn: (data: SignInData) => Promise<void>
 }
 
+interface CommonHeaderProperties extends HeadersDefaults {
+  Authorization: string;
+}
+
 export const AuthContext = createContext({} as AuthContextType)
 
 export function AuthProvider({ children }) {
-  const [user, setUser] = useState<User | null>(null);
-  const isAuthenticated = !!user;
+  const [user, setUser] = useState<User | null>(null)
+  const isAuthenticated = !!user
 
   /* useEffect(() => {
     const token = parseCookies(context)
@@ -41,21 +46,23 @@ export function AuthProvider({ children }) {
     const { data } = await fetchLogin({
       email,
       password
-    });
-    setCookie(null, 'atlas.token', data.token, {maxAge: 86400 * 7, path: '/'});
-    setCookie(null, 'atlas.id_store', data.id_store, {maxAge: 86400 * 7, path: '/'});
-    setCookie(null, 'atlas.first_name', data.first_name, {maxAge: 86400 * 7, path: '/'});
-    setCookie(null, 'atlas.id', data.id, {maxAge: 86400 * 7, path: '/'});
+    })
+    setCookie(null, 'atlas.token', data.token, { maxAge: 86400 * 7, path: '/' })
+    setCookie(null, 'atlas.id_store', data.id_store, { maxAge: 86400 * 7, path: '/' })
+    setCookie(null, 'atlas.first_name', data.first_name, { maxAge: 86400 * 7, path: '/' })
+    setCookie(null, 'atlas.id', data.id, { maxAge: 86400 * 7, path: '/' })
 
-    setCookie(null, 'food.isLoja', 'Abrir Loja', {maxAge: 86400 * 7, path: '/'});
+    setCookie(null, 'food.isLoja', 'Abrir Loja', { maxAge: 86400 * 7, path: '/' })
 
-    setUser(data);
+    setUser(data)
 
-    APIATLAS.defaults.headers['Authorization'] = `${data.token}`;
+    APIATLAS.defaults.headers = {
+      Authorization: `${data.token}`
+    } as CommonHeaderProperties
   }
 
   return (
-    <AuthContext.Provider value={{user, isAuthenticated, signIn}}>
+    <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
       {children}
     </AuthContext.Provider>
   )
